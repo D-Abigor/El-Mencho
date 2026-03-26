01d39252265d876c2364087eadaad1d74f132161
D-Abigor <D0Abigor@gmail.com>
2026-03-27 00:50:33 +0530

    Add custom exception classes for error handling

---
+++ exceptions.py
@@ -0,0 +1,11 @@
+class InvalidSession(Exception):
+  def __init__(self, message: str):
+    self.message = message
+
+class dbError(Exception):
+  def __init__(self, message: str):
+    self.message = message
+
+class couldNotGetUsernameAvailability(Exception):
+  pass
+  
