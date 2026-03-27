a6d5db1e007fb7bee3b37da16a7a9ae7f879e61c
D-Abigor <D0Abigor@gmail.com>
2026-03-27 15:37:27 +0530

    Create initial database schema for user and game management
    This SQL script creates multiple tables for user management, sessions, accounts, transactions, and game-related data, along with a sample user insertion.

---
+++ init.sql
@@ -0,0 +1,81 @@
+CREATE TABLE users(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    username text UNIQUE NOT NULL,
+    password_hash text NOT NULL,
+    access text NOT NULL CHECK ( access IN ('admin','user', 'booth'))
+);
+
+CREATE TABLE sessions(
+    session_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id uuid NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    expires_at TIMESTAMP NOT NULL,
+    CONSTRAINT sessions_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE accounts(
+    user_id uuid PRIMARY KEY,
+    balance INTEGER CHECK ( balance>=0),
+    CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id)
+ );
+
+CREATE TABLE transactions(
+    transaction_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    change INTEGER NOT NULL,
+    source uuid REFERENCES users(id) NOT NULL,
+    destination uuid REFERENCES users(id) NOT NULL,
+    processed_at TIMESTAMP DEFAULT now()
+ );
+
+
+
+CREATE TABLE gamesPlayed(
+  gameId UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  game TEXT NOT NULL,
+  timeOfFinish TIMESTAMP DEFAULT now(),
+  winner UUID NOT NULL,
+  CONSTRAINT winner_fkey FOREIGN KEY REFERENCES users(id)
+);
+
+CREATE TABLE activeGames(
+  user_id uuid PRIMARY KEY,
+  bet_amount INTEGER NOT NULL CHECK ( bet_amount >= /////// ),
+  game TEXT NOT NULL,
+  CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+CREATE TABLE gamePlayers(
+  ID SERIAL PRIMARY KEY, 
+  gameId UUID NOT NULL,
+  user_id UUID NOT NULL,
+  initial_bet INT NOT NULL,
+  final_amount INT NOT NULL CHECK ( final_amount >= 0),
+  timeOfFinish TIMESTAMP NOT NULL DEFAULT now(),
+  CONSTRAINT gameId_fkey FOREIGN KEY (gameId) REFERENCES gamesPlayed(gameId),
+  CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id),
+  CONSTRAINT finishTime_fkey FOREIGN KEY (timeOfFinish) REFERENCES gamesPlayed(timeOfFinish),
+);
+
+
+
+
+CREATE TABLE queue(
+  number SERIAL PRIMARY KEY,
+  user_id UUID NOT NULL,
+  game TEXT NOT NULL,
+  timeOfJoin TIMESTAMP DEFAULT now(),
+  CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+ INSERT INTO users(
+    username,
+    password_hash,
+    access
+    )
+    values (
+        'toastedmaster',
+        '$2b$12$ZN20i9eKmlJhged.2GU8Quw/l.IN9mnBhSMnFGwHO8hZScptufutW',
+        'admin'
+    );
