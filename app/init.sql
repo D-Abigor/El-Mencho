33b4547bde80dc0180ba4a2c442ead778be3e212
D-Abigor <D0Abigor@gmail.com>
2026-03-30 00:32:47 +0530

    Update status options in tables creation SQL

--- app/init.sql
+++ app/init.sql
@@ -37,7 +37,7 @@ CREATE TABLE transactions(
  CREATE TABLE tables(
   tableId INT PRIMARY KEY,
   gameSelected TEXT CHECK (gameSelected IN ('Teen Patti','Poker','3 of spades','Blackjack','Rummy','Crazy 8s')),
-  status TEXT CHECK ( status IN ('idle', 'running', 'ended')),
+  status TEXT CHECK ( status IN ('idle', 'ready','running', 'ended')),
   max_players INT
 )
 
