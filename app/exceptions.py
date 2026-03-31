ab055f19bd7c8dc32ee77318528dfc1cf95bd22b
D-Abigor <D0Abigor@gmail.com>
2026-03-31 05:58:57 +0530

    added exception handling call

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,18 +1,22 @@
 class InvalidSession(Exception):
     def __init__(self, message: str = "Your session is invalid or has expired."):
         self.message = message
+        super().__init__(message)
 
 class DbError(Exception):
-    def __init__(self, message: str):
+    def __init__(self, message: str = "db error"):
         self.message = message
+        super().__init__(message)
 
 class CouldNotGetUsernameAvailability(Exception):
     pass
 
 class AuthenticationFailure(Exception):
-    def __init__(self, message: str):
+    def __init__(self, message: str = "authentication failure"):
         self.message = message
+        super().__init__(message)
 
 class TransactionError(Exception):
-    def __init__(self, message: str):
+    def __init__(self, message: str = "transaction failed"):
         self.message = message
+        super().__init__(message)
